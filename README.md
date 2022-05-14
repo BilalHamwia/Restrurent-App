f7576b27faba4e29fc08b9f3d8db0a62329087ad
Bilal Hamwia <bilal.syrian1995@gmail.com>
2022-05-14 16:41:29 +0100

    Add project files.

---
+++ README.md
@@ -0,0 +1,37 @@
+
+# Resturant App
+
+Small restaurant management software developed by C# , design  xaml and database sql
+
+This program manages a small restaurant by placing orders quickly and helps customers
+
+These techniques were used because it is directed to the accountant's device and is intended for computers and does not need access to the web
+
+
+## Features
+
+- Add new food Items
+- Place new order
+- Updata order
+- Generate Bill
+
+
+## Installation
+
+You do not need anything to run, just download the program, or if you want to modify and develop, just make a clone repository
+## Support
+
+For support, email Bilal.syrian1995@gmail.com
+
+
+## Used By
+
+This project is used by the following :
+
+- Bilal Hamwia 
+
+
+## 🔗 Links
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](www.linkedin.com/in/bilal-hamwia-b856a5118)
+[![FaceBook](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://www.facebook.com/BilalHamwia/)
+
---
+++ Restrurent-Application-WPF/Model/sampleData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restrurent_Application_WPF.Model
+{
+
+    public class sampleData : DropCreateDatabaseAlways<RestrurentDB>
+    {
+        enum bookingstatus { Booked, Reserved, Available };
+        protected override void Seed(RestrurentDB context)
+        {
+            try
+            {
+
+                List<FoodItems> fooditems = new List<FoodItems>()
+                {
+                    new FoodItems { FoodName = "Tea", Description = "Tea", fPrice = 8 },
+                     new FoodItems { FoodName = "Samosa", Description = "Samosa", fPrice = 12 },
+                      new FoodItems { FoodName = "Potato", Description = "Potato", fPrice = 12 },
+                       new FoodItems { FoodName = "Grilled chicken", Description = "Grilled chicken", fPrice = 20 }
+                };
+
+                List<TableList> tablelist = new List<TableList>()
+                {
+                    new TableList { TableName = "Table 1", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList {  TableName = "Table 2", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList {  TableName = "Table 3", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList {  TableName = "Table 4", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList {  TableName = "Table 5", BookingStatus = bookingstatus.Booked.ToString() },
+                    new TableList {  TableName = "Table 6", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList {  TableName = "Table 7", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList {  TableName = "Table 8", BookingStatus = bookingstatus.Available.ToString() },
+                     new TableList {  TableName = "Table 9", BookingStatus = bookingstatus.Available.ToString() }
+                };
+
+                foreach (FoodItems f in fooditems)
+                {
+                    context.FoodItems.Add(f);
+                }
+
+                foreach (TableList t in tablelist)
+                {
+                    context.TableList.Add(t);
+                }
+
+                context.SaveChanges();
+                base.Seed(context);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
